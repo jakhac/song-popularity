9cccc3f6ebdf3d3c54bd07e459e2e4e099a804ff
Kevin Katzkowski <katzkows@uni-bremen.de>
2021-06-18 17:10:50 +0200

    Implemented basic lyrics getter

--- src/__main__.py
+++ src/__main__.py
@@ -1,11 +1,12 @@
 import logging
 import logging.config
-from .logging_config import LOGGING_CONFIG
 import sys
 
-from .lyrics_dataset.lyrics_api import connect_to_api
+from .logging_config import LOGGING_CONFIG
+from .lyrics_dataset.lyrics_api import connect_to_api, run_lyrics_getter
 
 logging.config.dictConfig(LOGGING_CONFIG)
+
 
 def main():
     logging.config.dictConfig(LOGGING_CONFIG)
@@ -20,13 +21,27 @@ def main():
     # get script name
     script = sys.argv[1]
 
+    # get script arguments
+    args = sys.argv[2:]
+
+    if len(args) == 0:
+        log.info("No command line arguments passed")
+    else:
+        log.info(f"Passed {len(args)} command line arguments ")
+
     if script == "connect_to_api":
         log.info("Calling script 'connect_to_api'")
         connect_to_api()
+    elif script == "run_lyrics_getter":
+        if len(args) != 0:
+            log.info(f"Calling script 'run_lyrics_getter' with argument '{args[0]}'")
+            run_lyrics_getter(str(args[0]))
+        else:
+            log.info("Calling script 'run_lyrics_getter'")
+            run_lyrics_getter()
     else:
         log.critical(f"Unknown script '{script}', exiting program.")
         exit(1)
-
 
 
 if __name__ == "__main__":
